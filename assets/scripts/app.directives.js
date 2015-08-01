becbb867a6c905919614ab7bb7bb287c68fb7029
Mike Burden <maburdenjr@gmail.com>
2015-08-01 01:44:44 -0700

    Dynamically add children

--- assets/scripts/app.directives.js
+++ assets/scripts/app.directives.js
@@ -1,25 +1,22 @@
 (function () {
     angular.module('app')
-        .directive('person', modifyTree);
-
-    function modifyTree() {
-        var directive = {
-            restrict: 'C',
-            link: link
-        }
-
-        return directive;
-
-        function link(scope, ele, attrs) {
-            ele.on('click', function(event) {
-                event.stopPropagation();
-                var newChild = "<li><a href='#' class='person editme' contenteditable='true'>New Child</a></li>";
-                if ($(this).data('childCount') == 0) {
-                } else {
-                    $(this).siblings('.children').append(newChild);
+        .directive('person', function($compile) {
+            return {
+                restrict: 'C',
+                link: function(scope, ele, attrs) {
+                    ele.on('dblclick', function(event) {
+                        event.stopPropagation();
+                        var newSibling = "<li><a href='#' class='person editme' contenteditable='true'>New Child</a></li>";
+                        var newFamily = "<ul class='children'>"+newSibling+"</ul>";
+                        if ($(this).data('childCount') == 0) {
+                            $(this).after($compile(newFamily)(scope));
+                        } else {
+                            $(this).siblings('.children').append($compile(newSibling)(scope));
+                        }
+                        scope.renderTree();
+                    })
                 }
-            })
-        }
-    }
+            }
+        });
 
 })();
--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,39 +5,38 @@
 
     function FamilyTree($scope, $rootScope) {
 
-        var tree = evaluateTree();
-        console.log(tree);
+        $scope.renderTree = function() {
+            var familyTree = {
+                noChildren: [],
+                noSiblings: [],
+                mostGrandchildren: null
+            }
 
-    }
+            var grandChildArray = [];
 
-    function evaluateTree() {
-        var familyTree = {
-            noChildren: [],
-            noSiblings: [],
-            mostGrandchildren: null
+            $('.person').each(function(index) {
+                var childMatch = $(this).siblings('.children').children('li');
+                var childCount = childMatch.size();
+                var siblingCount = $(this).parent().siblings('li').size();
+                var grandchildCount = childMatch.children('ul').children('li').size();
+                var personName = $(this).text();
+
+                $(this).data('childCount', childCount);
+
+                if (childCount === 0) {
+                    familyTree.noChildren.push(personName);
+                }
+                if (siblingCount === 0) {
+                    familyTree.noSiblings.push(personName);
+                }
+                grandChildArray[grandchildCount] = personName;
+            })
+            familyTree.mostGrandchildren = grandChildArray[grandChildArray.length-1];
+            return familyTree;
         }
 
-        var grandChildArray = [];
-
-        $('.person').each(function(index) {
-            var childMatch = $(this).siblings('.children').children('li');
-            var childCount = childMatch.size();
-            var siblingCount = $(this).parent().siblings('li').size();
-            var grandchildCount = childMatch.children('ul').children('li').size();
-            var personName = $(this).text();
-
-            $(this).data('childCount', childCount);
-
-            if (childCount === 0) {
-                familyTree.noChildren.push(personName);
-            }
-            if (siblingCount === 0) {
-                familyTree.noSiblings.push(personName);
-            }
-            grandChildArray[grandchildCount] = personName;
-        })
-        familyTree.mostGrandchildren = grandChildArray[grandChildArray.length-1];
-        return familyTree;
+        var relationships = $scope.renderTree();
     }
+
 
 })();
