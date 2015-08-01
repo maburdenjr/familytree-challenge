9baaca86c1496d51d954bb9d9d0f5d5fd813a7b3
Mike Burden <maburdenjr@gmail.com>
2015-08-01 12:01:41 -0700

    Adding the ability to remove members from the family tree

--- assets/scripts/app.directives.js
+++ assets/scripts/app.directives.js
@@ -4,6 +4,7 @@
             return {
                 restrict: 'C',
                 link: function(scope, ele, attrs) {
+                    // Double click to create new child element
                     ele.on('dblclick', function(event) {
                         event.stopPropagation();
                         var newSibling = "<li><a href='#' class='person editme' contenteditable='true'>New Child</a></li>";
@@ -15,6 +16,23 @@
                         }
                         scope.renderTree();
                     })
+
+                    //Hit escape to delete child element
+                    ele.on('keyup', function(event) {
+                        event.stopPropagation();
+                        var personName = $(this).text();
+                        if(event.which == 27){
+                            var deleteElement = confirm("Are you sure you want to remove "+personName+" and his/her descendants from the family tree.");
+                            if (deleteElement == true) {
+                                if ($(this).parent().parent().siblings('.person').data('childCount') >= 1) {
+                                    $(this).parent().remove();
+                                } else {
+                                    $(this).parent().parent().remove();
+                                }
+                                scope.renderTree();
+                            }
+                        }
+                    });
                 }
             }
         });
